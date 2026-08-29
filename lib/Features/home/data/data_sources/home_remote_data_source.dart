11379415461c6a02cfd47ef79d83246e31d311a7
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-08-29 18:08:30 +0200

    implement featch feature books and newest books remaote data source

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,41 @@
+import 'package:bookly/Features/home/data/models/book_model.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+import 'package:bookly/core/utils/api_services.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> featchFeaturBooks();
+  Future<List<BookEntity>> featchNewestBooks();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+  final ApiServices apiServices;
+
+  HomeRemoteDataSourceImpl(this.apiServices);
+  @override
+  Future<List<BookEntity>> featchFeaturBooks()async {
+    var data = await apiServices.get(endpoint: 'flutter');
+
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+ 
+
+  @override
+  Future<List<BookEntity>> featchNewestBooks()async {
+    var data = await apiServices.get(endpoint: 'flutter');
+
+    List<BookEntity> books = getBooksList(data);
+    return books;
+ 
+  }
+   List<BookEntity> getBooksList(Map<String, dynamic> data) {
+     List<BookEntity> books= [];
+    for (var item in data['item']) {
+      books.add(BookModel.fromJson(item));
+      
+    }
+    return books;
+  }
+}
