27638cc79a068ab54f44af72f54008ba99ed42b9
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-08-31 22:56:49 +0200

    generate booentity adaptor

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,12 +1,20 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookid;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String? auther;
+  @HiveField(3)
   final String? image;
-
+  @HiveField(4)
   final num? rating;
-
+  @HiveField(5)
   final String? saleability;
+  @HiveField(6)
   final num? averageRatingbook;
 
   BookEntity(
@@ -15,5 +23,6 @@ class BookEntity {
       required this.image,
       required this.rating,
       required this.bookid,
-      required this.saleability,required this.averageRatingbook});
+      required this.saleability,
+      required this.averageRatingbook});
 }
