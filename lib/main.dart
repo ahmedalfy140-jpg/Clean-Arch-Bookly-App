12f320fe6b41199798a17b4e2d536fd447c18f85
Ahmed Alfy <ahmedalfy140@gmail.com>
2026-09-05 13:34:11 +0200

    create simple bloc oserver

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:bookly/Features/home/presentation/manger/feature_book_cubit/feat
 import 'package:bookly/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/bloc_observer.dart';
 import 'package:bookly/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,13 +15,15 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
-  runApp(const Bookly());
+
   await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   setupServiceLocator();
 
   await Hive.openBox(kFeaturedBook);
   await Hive.openBox(kNewestBook);
+  Bloc.observer=SimpleBlocObserver();
+    runApp(const Bookly());
 }
 
 final getIt = GetIt.instance;
